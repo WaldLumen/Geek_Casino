56193450a11bd5f96b17dc9308a7281ea1aa226e
silvia <satotanakaiso@gmail.com>
2022-07-22 00:57:12 +0200

    Rename and add some defs

--- tgbot/bot/Banque.py
+++ tgbot/bot/Banque.py
@@ -1,12 +1,8 @@
-import os
-
 from psycopg_pool import ConnectionPool
 
 
-class Dwarf:
+class Banque:
     def __init__(self):
-        self.name = os.getenv('name')
-        self.user = os.getenv('user')
         self.pool = ConnectionPool("dbname=Geek_Casino user=postgres")
 
     def create_table(self):
@@ -19,21 +15,12 @@ class Dwarf:
                                 cash integer) 
                             """)
 
-    def dices(self, user_id):
+    def drop_table(self):
         with self.pool.connection() as conn:
             with conn.cursor() as cur:
-                cur.execute("SELECT cash FROM users WHERE"
-                            " id = %s FOR UPDATE" % user_id)
-                return cur.fetchone()[0]
-
-    def profile(self, user_id):
-        with self.pool.connection() as conn:
-            with conn.cursor() as cur:
-                cur.execute("SELECT cash FROM users WHERE id = %s" % user_id)
-                return cur.fetchone()
+                cur.execute("DROP TABLE users")
 
     def register_user(self, user_id):
-
         with self.pool.connection() as conn:
             with conn.cursor() as cur:
                 cur.execute("SELECT id FROM users WHERE ID = %s" % user_id)
@@ -45,10 +32,21 @@ class Dwarf:
                 else:
                     pass
 
+    def show_cash(self, user_id):
+        with self.pool.connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute("SELECT cash FROM users WHERE id = %s" % user_id)
+                return cur.fetchone()[0]
+
+    def profile(self, user_id):
+        with self.pool.connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute("SELECT cash FROM users WHERE id = %s" % user_id)
+                return cur.fetchone()
+
     def replenishment(self, ball: int, user_id: int):
         with self.pool.connection() as conn:
             with conn.cursor() as cur:
-                cur.execute("SELECT id FROM users WHERE id = %s" % user_id)
                 cur.execute(
                     "Update users set cash = cash + %s WHERE id = %s" % (ball, user_id)
                 )
@@ -56,12 +54,21 @@ class Dwarf:
     def cash_withdrawal(self, ball: int, user_id: int):
         with self.pool.connection() as conn:
             with conn.cursor() as cur:
-                cur.execute("SELECT id FROM users WHERE id = %s" % user_id)
                 cur.execute(
                     "Update users set cash = cash - %s WHERE id = %s" % (ball, user_id)
                 )
 
-    def drop_table(self):
+    def start_transaction(self):
         with self.pool.connection() as conn:
             with conn.cursor() as cur:
-                cur.execute("DROP TABLE users")
+                cur.execute("BEGIN")
+
+    def commit_transaction(self):
+        with self.pool.connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute("COMMIT")
+
+    def select_cash_for_update(self, user_id):
+        with self.pool.connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute("SELECT cash FROM users WHERE id = %s FOR UPDATE" % user_id)
