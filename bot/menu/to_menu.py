0859292aec277dd61b02f61bee52471d6ac717cd
Sylvia Lumen <sylvia_lumen@proton.me>
2023-01-04 14:22:33 +0200

    Optimizing imports
    Changes to be committed:
    	modified:   menu/to_menu.py

--- bot/menu/to_menu.py
+++ bot/menu/to_menu.py
@@ -1,23 +1,23 @@
-from aiogram import types
+from aiogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
 
 
-async def to_main_menu(call: types.CallbackQuery):
+async def to_main_menu(edit_message: CallbackQuery.message.edit_text):
     # main menu
-    keyboard = types.InlineKeyboardMarkup(row_width=2)
-    keyboard.add(types.InlineKeyboardButton(text="👤Profile👤", callback_data="profile"),
-                 types.InlineKeyboardButton(text="⭐Start Game⭐", callback_data="games"),
-                 types.InlineKeyboardButton(text="🔝Top🔝", callback_data="f"))
+    keyboard = InlineKeyboardMarkup(row_width=2)
+    keyboard.add(InlineKeyboardButton(text="👤Profile👤", callback_data="profile"),
+                 InlineKeyboardButton(text="⭐Start Game⭐", callback_data="games"),
+                 InlineKeyboardButton(text="🔝Top🔝", callback_data="f"))
 
-    await call.message.edit_text("Welcome. This is main menu of this bot,"
+    await edit_message("Welcome. This is main menu of this bot,"
                                  "here you can check your profile or start playing.", reply_markup=keyboard)
 
 
-async def to_help_menu(call: types.CallbackQuery):
+async def to_help_menu(edit_message: CallbackQuery.message.edit_text):
     buttons = [
-        types.InlineKeyboardButton(text="Games Base Information", callback_data="g_b_i")
+        InlineKeyboardButton(text="Games Base Information", callback_data="g_b_i")
     ]
 
-    keyboard = types.InlineKeyboardMarkup(row_width=2)
+    keyboard = InlineKeyboardMarkup(row_width=2)
     keyboard.add(*buttons)
 
-    await call.message.edit_text("Hello, I'm your informant, what do you want to know?", reply_markup=keyboard)
+    await edit_message("Hello, I'm your informant, what do you want to know?", reply_markup=keyboard)
